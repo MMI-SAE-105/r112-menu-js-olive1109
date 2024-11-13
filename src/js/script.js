3c98f4084be0f84522bdc41cfc601b0b3ef9198e
Noëlie Ahuir <noelie.ahuir@edu.univ-fcomte.fr>
2024-11-13 11:39:18 +0100

    classe noscroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
 const toggle = document.querySelector(".menu-btn");
 const nav = document.querySelector("nav.menu");
+const body = document.querySelector("body");
 
 toggle.addEventListener("click", () => {
     const isOpen = toggle.ariaExpanded === "true";
     const isClosed = !isOpen;
     console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
     nav.ariaHidden = isOpen;
-    toggle.ariaExpanded = isClosed
+    toggle.ariaExpanded = isClosed;
+    body.classList.toggle("noscroll", isClosed); 
 });
